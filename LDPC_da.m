a160f155079d855eafc3b281e13df44410ff5c5e
олегсий <78318213+lex4z@users.noreply.github.com>
2025-03-31 12:04:36 +0300

    Update LDPC_da.m

--- LDPC_da.m
+++ LDPC_da.m
@@ -23,12 +23,82 @@ for f = 1:num_frames
 end
 
 %%
-t_words = reshape(codewords,length(codewords)/block_length,block_length);
+info = codewords;
+K = length(info);
+EbN0_dB = 2;%-4:1:10;
+EbN0 = 10.^(EbN0_dB./10);
+Pb = 1;
+noise_var = sqrt(Pb./(2.*EbN0));
+iter_num = 1:30;
+
+tx_sig = info*(-2)+1;
+rx_sig = zeros(length(iter_num), K);
+for i = 1:length(iter_num)
+    rx_sig(i,:) = tx_sig + noise_var(1)*randn(size(tx_sig));
+end
 
 %%
-info = t_words(1,:);
-K = length(info);
-EbN0_dB = -4:0.5:10;
+for i=1:min(size(pcmatrix))
+  for j=1:max(size(pcmatrix))
+      if pcmatrix(i,j) == true
+         t(i,j)=1;
+      else
+         t(i,j)=0;
+      end
+   end
+end
+% %%
+% rx_info = zeros(length(noise_var), num_info_bits*num_frames);
+% 
+% for i = 1:length(noise_var)
+%      for j = 1:(K/block_length)
+%         rx_info(i,(num_info_bits*(j-1) + 1):num_info_bits*j) = LDPC_decoder(rx_sig(i,(block_length*(j-1) + 1):block_length*j),num_info_bits,t,5);
+%      end
+% end
+
+%%
+rx_info = zeros(length(iter_num), num_info_bits*num_frames);
+
+for i = 1:length(iter_num)
+     for j = 1:(K/block_length)
+        rx_info(i,(num_info_bits*(j-1) + 1):num_info_bits*j) = LDPC_decoder(rx_sig(1,(block_length*(j-1) + 1):block_length*j),num_info_bits,t,iter_num(i));
+     end
+end
+
+%%
+
+razn = info_bits'-rx_info;
+error_cnt = zeros(1,length(iter_num));
+for i = 1:length(iter_num)
+    error_cnt(i) = sum(razn(i,:)~=0);
+end
+
+error_p = error_cnt/K;
+
+% %% график
+% figure
+% semilogy(EbN0_dB,error_p)
+% grid on
+% xlim([min(EbN0_dB) max(EbN0_dB)])
+% ylim([1e-5 1])
+% ylabel("BER")
+
+%% график
+figure
+semilogy(iter_num,error_p)
+grid on
+xlim([min(iter_num) max(iter_num)])
+%ylim([1e-5 1])
+%ylabel("BER")
+
+
+%%
+K = 1e6;
+info = randi([0 1],1,K);
+
+%reshape(t,3,length(t)/3)
+
+EbN0_dB = -4:1:10;
 EbN0 = 10.^(EbN0_dB./10);
 Pb = 1;
 noise_var = sqrt(Pb./(2.*EbN0));
@@ -38,38 +108,28 @@ rx_sig = zeros(length(noise_var), K);
 for i = 1:length(noise_var)
     rx_sig(i,:) = tx_sig + noise_var(i)*randn(size(tx_sig));
 end
+%rx_sig = tx_sig + noise_var*randn(size(tx_sig));
+rx_info = (1-sign(rx_sig))/2;
 
-%%
-for i=1:min(size(pcmatrix))
-  for j=1:max(size(pcmatrix))
-      if pcmatrix(i,j) == true
-         t(i,j)=1;
-      else
-          t(i,j)=0;
-      end
-   end
-end
-%%
-rx_info = zeros(length(noise_var), K);
-for i = 1:length(noise_var)
-     rx_info(i,1:num_info_bits) = LDPC_decoder(rx_sig(i,:),num_info_bits,t,5);
-end
+%rx_info = zeros(size(tx_sig));
+%rx_info(rx_sig<0) = 1;
 
-
-%%
-
-razn = info-rx_info;
+t = info-rx_info;
 error_cnt = zeros(1,length(noise_var));
 for i = 1:length(noise_var)
-    error_cnt(i) = sum(razn(i,1:num_info_bits)~=0);
+    error_cnt(i) = sum(t(i,:)~=0);
 end
 
 error_p = error_cnt/K;
 
 %% график
-figure
+hold on
 semilogy(EbN0_dB,error_p)
 grid on
 xlim([min(EbN0_dB) max(EbN0_dB)])
 ylim([1e-5 1])
 ylabel("BER")
+hold off
+
+
+%histogram(info)

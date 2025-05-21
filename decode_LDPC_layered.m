b720b56bc21c6a2db990ddc7855a5c0a7d93b35c
олегсий <78318213+lex4z@users.noreply.github.com>
2025-05-21 11:25:45 +0300

    Update decode_LDPC_layered.m

--- decode_LDPC_layered.m
+++ decode_LDPC_layered.m
@@ -2,23 +2,27 @@ function data_out = decode_LDPC_layered(data_in, num_of_bits, H_matrix, iter_num
     llr_vector = data_in; 
     H_matrix = H_matrix == 1;
     L_matrix = zeros(size(H_matrix));
-    
+
+
     for iter = 1:iter_num
-        for i = 1:ef:min(size(L_matrix))
+        for i = 1:ef:size(H_matrix,1)
+            rows = i:(i+ef-1);
             if(iter == 1) 
-                L_matrix(i:(i+ef-1),:) = H_matrix(i:(i+ef-1),:).*llr_vector;
+                L_matrix(rows,:) = H_matrix(rows,:).*llr_vector;
             else
-                L_matrix(i:(i+ef-1), :) = (llr_vector - L_matrix(i:(i+ef-1), :)).*H_matrix(i:(i+ef-1),:);
-                llr_vector = sum(L_matrix(i:(i+ef-1),:),1);
+                L_matrix(rows, :) = (llr_vector - L_matrix(rows, :)).*H_matrix(rows,:);
+                H_not_0 = sum(H_matrix(rows,:),1) == 1;
+                llr_vector(H_not_0) = sum(L_matrix(rows,H_not_0),1);
             end
+
             for j = i:(i+ef-1)
                 L_matrix(j,H_matrix(j,:)) = row_op(L_matrix(j,H_matrix(j,:)));
             end
-            
-            llr_vector = llr_vector + sum(L_matrix(i:(i+ef-1),:),1);
+
+            llr_vector = llr_vector + sum(L_matrix(rows,:),1);
         end   
     end
-    
+
     res = (1-sign(llr_vector))/2;
     data_out = res(1:num_of_bits);
 end
